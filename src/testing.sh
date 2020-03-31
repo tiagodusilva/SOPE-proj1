3754d53e402bdb69dfcbe2ee3244c88f586d8d92
tiago <tiagodusilva@gmail.com>
2020-03-31 15:48:08 +0100

    Improved testing

--- src/testing.sh
+++ src/testing.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+simpledu_location="${HOME}/Documents/SOPE/SOPE-proj1/src/simpledu"
+
+directories_file="${HOME}/Documents/SOPE/SOPE-proj1/src/directories_to_test.txt"
+
 command_args=(
     "-l -a"                          # 0
     "-l -a -L"                       # 1
@@ -13,24 +17,31 @@ command_args=(
     "-l -a -L -S -B 25"              # 9
     "-l -a -B 25"                    # 10
     "-l -a -b"                       # 11
+    "-l"                          # 12
+    "-l -L"                       # 13
+    "-l -L --max-depth=0"         # 14
+    "-l -L -S --max-depth=0"      # 15
+    "-l -L --max-depth=1"         # 16
+    "-l -L -S --max-depth=1"      # 17
+    "-l -L --max-depth=2"         # 18
+    "-l -L -S --max-depth=1"      # 19
+    "-l -L -B 25"                 # 20
+    "-l -L -S -B 25"              # 21
+    "-l -B 25"                    # 22
+    "-l -b"                       # 23
 )
 
-directories=(
-    ${HOME}/Documentos/SOPE/SOPE-proj1/src/
-  
-)
-
-for dir in ${directories[*]}
+while read dir;
 do
     echo "Dir: ${dir}"
     for i in ${!command_args[*]}
     do
         echo -n "Test no. $i: "
-        (./simpledu ${command_args[$i]} ${dir} || echo $?) | sort -k2 > /tmp/testeT_simpledu.txt
+        (${simpledu_location} ${command_args[$i]} ${dir} || echo $?) | sort -k2 > /tmp/testeT_simpledu.txt
         (du ${command_args[$i]} ${dir} || echo $?) | sort -k2 > /tmp/testeT_du.txt
         diff -q /tmp/testeT_simpledu.txt /tmp/testeT_du.txt > /dev/null 2>&1 && echo OK || echo FAILED
         
         # Uncomment line below to print what's wrong (in case of test failed)
         # diff -y /tmp/testeT_simpledu.txt /tmp/testeT_du.txt
     done
-done
+done < $directories_file
